4713c7a62fc2686088b7cff44e21c2e397e0d486
Rubén Villar Escudero <yo@rubenvillar.com>
2019-02-26 22:04:15 +0100

    Initial commit

---
+++ src/main/java/com/rubenvillar/poc/openapicontractsserver/OpenapiContractsServerApplication.java
@@ -0,0 +1,13 @@
+package com.rubenvillar.poc.openapicontractsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiContractsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiContractsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubenvillar/poc/openapicontractsserver/OpenapiContractsServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rubenvillar.poc.openapicontractsserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenapiContractsServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
